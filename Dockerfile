25c969519217acbfcd517edc3eb5dbcb1d2d06e2
Gautam <gk903138@gmail.com>
2026-04-17 20:43:53 +0530

    [Gautam] : Modified Dockerfile and nginx.conf files for seemless Microservies communication of frontend with backend

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Stage 1: Build Angular app
+FROM node:20 AS build
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+RUN npm run build -- --configuration=production
+
+# Stage 2: Serve with Nginx
+FROM nginx:alpine
+
+RUN rm -rf /usr/share/nginx/html/*
+
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+# ✅ FIXED PATH (browser folder)
+COPY --from=build /app/dist/quantity-ui/browser /usr/share/nginx/html
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  gatewayBaseUrl: 'http://localhost:8080',
-  apiUrl: 'http://localhost:8080/api',
+  gatewayBaseUrl: 'http://api-gateway:8080',
+  apiUrl: 'http://api-gateway:8080/api',
   authEndpoints: {
     login: '/auth/login',
     register: '/auth/signup',
@@ -11,7 +11,7 @@ export const environment = {
     resetPassword: '/auth/reset-password'
   },
   oauth: {
-    googleAuthorizeUrl: 'http://localhost:8080/oauth2/authorize/google'
+    googleAuthorizeUrl: 'http://api-gateway:8080/oauth2/authorize/google'
   },
   storage: {
     authSessionKey: 'qm_auth_session'
